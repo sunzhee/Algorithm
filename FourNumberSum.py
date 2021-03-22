468ee5be976571c29a99f6ca4792e64a09704bce
sunzhee <sunzhee@gmail.com>
2021-03-21 21:30:03 -0700

    Mar21

--- FourNumberSum.py
+++ FourNumberSum.py
@@ -1,3 +1,24 @@
+"""
+Four Number Sum
+Write a function that takes in a non-empty array of distinct
+integers and an integer representing a target sum. The function
+should nd all quadruplets in the array that sum up to the target
+sum and return a two-dimensional array of all these quadruplets
+in no particular order.
+If no four numbers sum up to the target sum, the function
+should return an empty array.
+
+Sample Input
+array = [7, 6, 4, -1, 1, 2]
+targetSum = 16
+
+Sample Output
+[[7, 6, 4, -1], [7, 6, 1, 2]] // the quadruplets
+
+"""
+
+
+#input
 array=[5,-5,-2,2,3,-3]
 targetSum = 0
 
@@ -26,6 +47,9 @@ def fourNumberSum(array, targetSum):
 		
 
 
-
-
+print("input array:")
+print(array)
+print("input targetSum:",targetSum)
+print("-------")
+print("output:")
 print (fourNumberSum(array,targetSum))
