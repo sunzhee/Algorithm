3686ad55a319c812b8ad880c351fc5a9a00d6b4f
Sunzhee <sunzhee@gmail.com>
2021-06-12 11:47:06 -0700

    Update selectionSort.py

--- sorting/selectionSort.py
+++ sorting/selectionSort.py
@@ -28,15 +28,12 @@ array = [8, 5, 2, 9, 5, 6, 3]
 
 #O(n^2) time, O(1) space
 def selectionSort(array):
-	currentId = 0
-	for currentId in range(0,len(array) - 1):
-		smallestId = currentId
-		for i in range(currentId + 1,len(array)):
-			if array[smallestId] > array[i]:
-				smallestId = i
-		#swap
-		array[smallestId],array[currentId] = array[currentId],array[smallestId]
-		currentId += 1
+	for currentIdx in range(len(array)-1):
+		smallestIdx = currentIdx
+		for j in range(currentIdx + 1,len(array)):
+			if array[j] < array[smallestIdx]:
+				smallestIdx = j
+		array[smallestIdx],array[currentIdx] = array[currentIdx],array[smallestIdx]
 	return array
 
 print("input:",array)
