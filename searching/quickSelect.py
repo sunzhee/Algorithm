2ee8ec09bc0e5d0e545682ff5913279b78509608
Sunzhee <sunzhee@gmail.com>
2021-07-01 23:00:33 -0700

    Update quickSelect.py

--- searching/quickSelect.py
+++ searching/quickSelect.py
@@ -30,6 +30,10 @@ def quickSelectHelper(array,startIndex,endIndex,position):
 		pivotIndex = startIndex
 		leftIndex = startIndex + 1
 		rightIndex = endIndex
+		# 快速排序的核心部分，如果左边大于pivot同时右边小于pivot，则交换左右
+		# 如果左边小于等于pivot，左边位置ok，继续下一个 +=1
+		# 如果右边大于等于pivot，右边位置ok,继续下一个 -=1
+		# 左坐标大于右坐标，此次循环完成，右坐标和pivot交换位置
 		while leftIndex <= rightIndex:
 			if array[leftIndex] > array[pivotIndex] and array[rightIndex] < array[pivotIndex]:
 				swap(leftIndex,rightIndex,array)
