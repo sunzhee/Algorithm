5b13e78c6eb146ee66778fd7953e7e2d5a0dddd2
Sunzhee <sunzhee@gmail.com>
2021-07-03 01:40:48 -0700

    Update quickSort.py

--- sorting/quickSort.py
+++ sorting/quickSort.py
@@ -34,14 +34,17 @@ def quickSortHelper(array,startIndex,endIndex):
 	
 	while rightIndex >= leftIndex:
 		# 必须要先判断左右两边和pivot的值，如果符合条件就要立刻交换，然后再左右指针往中间走
+		# 快速排序核心算法，有三个关键步骤：
+		#  第一，如果左边大于pivot的同时，右边小于pivot那么，左右交换
+		#  第二，如果左边小于等于pivot，说明左边在合理位置，那么左边不操作，向中间移动一位 +=1
+		#  第三，如果右边大于等于pivot，说明右边在合理位置，右边不操作，向中间移动一位，-=1
+		#  全部完成之后，pivot和右边交换
 		if array[leftIndex] > array[pivotIndex] and array[rightIndex] < array[pivotIndex]:
 			swap(leftIndex,rightIndex,array)		
 		if array[leftIndex] <= array[pivotIndex]:
 			leftIndex += 1
 		if array[rightIndex] >= array[pivotIndex]:
 			rightIndex -= 1
-
-			
 	swap(rightIndex,pivotIndex,array)
 	leftIsSmaller = rightIndex -1 - startIndex < endIndex - (rightIndex + 1)
 	if leftIsSmaller:
