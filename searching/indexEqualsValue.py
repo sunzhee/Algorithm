2c953957f956e46ad62ab2c851efd5aaa48879bd
Sunzhee <sunzhee@gmail.com>
2021-07-07 23:53:52 -0700

    Update indexEqualsValue.py

--- searching/indexEqualsValue.py
+++ searching/indexEqualsValue.py
@@ -19,3 +19,29 @@ Sample Output
 
 """
 
+# O(log(n)) time, O(log(n)) space
+# 用binarySearch方法，实现log(n)，因为整个数组是 sorted array of distinct integers
+# 主要判断三种情况： 
+#	如果当前value小于index，说明可以排除包括它在内的全部左边的
+#	如果等于value，同时如果index == 0则肯定就是，因为index不会等于负值
+#	如果等于value，同时index-1的value	小于index-1，则肯定就是，因为可以排除全部左边的
+#	剩下所有情况，都可以排除全部右边的，继续递归
+def indexEqualsValue(array):
+	return indexEqualsValueHelper(0,len(array)-1,array)
+
+def indexEqualsValueHelper(leftIndex,rightIndex,array):
+	if leftIndex > rightIndex:
+		return -1
+	middleIndex = (leftIndex + rightIndex) // 2
+	middleValue = array[middleIndex]
+	
+	if middleValue < middleIndex:
+		return indexEqualsValueHelper(middleIndex + 1,rightIndex,array)
+	elif middleValue == middleIndex and middleIndex == 0:
+		return middleIndex
+	elif middleValue == middleIndex and  array[middleIndex - 1] < middleIndex - 1:
+		return middleIndex
+	else:
+		return indexEqualsValueHelper(leftIndex,middleIndex - 1,array)
+
+
