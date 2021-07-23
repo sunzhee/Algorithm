e4c0320b314ebd08a24f2ba3c92d5cc6db0ad233
Sunzhee <sunzhee@gmail.com>
2021-07-23 15:03:50 -0700

    Create mergePDF.py

---
+++ myTest/mergePDF.py
@@ -0,0 +1,17 @@
+"""
+merge multi pdf files to one
+
+"""
+
+import os
+from PyPDF2 import PdfFileMerger
+
+target_path = '/Users/kwsy/Desktop'
+pdf_lst = [f for f in os.listdir(target_path) if f.endswith('.pdf')]
+pdf_lst = [os.path.join(target_path, filename) for filename in pdf_lst]
+
+file_merger = PdfFileMerger()
+for pdf in pdf_lst:
+    file_merger.append(pdf)     # 合并pdf文件
+
+file_merger.write("/Users/kwsy/Desktop/merge.pdf")
