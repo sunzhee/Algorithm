de8a4be4d8bde4023f42ea448380302f420276a4
Sunzhee <sunzhee@gmail.com>
2021-07-23 23:32:58 -0700

    Update mergePDF.py

--- myTest/mergePDF.py
+++ myTest/mergePDF.py
@@ -11,12 +11,12 @@ pip install PyPDF2
 import os 
 from PyPDF2 import PdfFileMerger
 
-target_path = '/Users/kwsy/Desktop'
+target_path = './'
 pdf_lst = [f for f in os.listdir(target_path) if f.endswith('.pdf')]
 pdf_lst = [os.path.join(target_path, filename) for filename in pdf_lst]
 
 file_merger = PdfFileMerger()
 for pdf in pdf_lst:
-    file_merger.append(pdf)     # 合并pdf文件
+    file_merger.append(pdf)
 
-file_merger.write("/Users/kwsy/Desktop/merge.pdf")
+file_merger.write("./merge.pdf")
